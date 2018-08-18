270927e70a6b46c2eb3ca920adefcf923f0401ab
海城高校情報科 <kaijoITCourse@202.171.149.2.static.zoot.jp>
2018-08-18 09:30:56 +0900

    Initial Commit

---
+++ Brain Training/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Brain Training
+//
+//  Created by 海城高校情報科 on 2018/08/18.
+//  Copyright © 2018年 海城高校情報科. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Brain TrainingTests/Brain_TrainingTests.swift
@@ -0,0 +1,36 @@
+//
+//  Brain_TrainingTests.swift
+//  Brain TrainingTests
+//
+//  Created by 海城高校情報科 on 2018/08/18.
+//  Copyright © 2018年 海城高校情報科. All rights reserved.
+//
+
+import XCTest
+@testable import Brain_Training
+
+class Brain_TrainingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
